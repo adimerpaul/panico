2be61964a61958871e0fed04c36b27abe0695b27
adimerpaul <adimer101@gmail.com>
2022-11-17 00:10:50 -0400

    Update index.js

--- chat-example/index.js
+++ chat-example/index.js
@@ -5,7 +5,7 @@ const io = require('socket.io')(http, {
     origin: "*",
   },
 });
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
